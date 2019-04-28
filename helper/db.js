ebea192fb49d9d164e939e37cec3f85c7ff03be6
Muammer Top <muammertop@outlook.com>
2019-04-28 18:42:17 +0300

    Initial commit

---
+++ helper/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+module.exports = () =>{
+	mongoose.connect('mongodb://movie_user:abcd1234@ds147926.mlab.com:47926/heroku_8d01t5gc', { useNewUrlParser: true });
+
+	mongoose.connection.on('open', () => {
+		console.log('MongoDB: Connected')
+	});
+
+	mongoose.connection.on('error', (err) => {
+		console.log('MongoDB: Error ', err);
+	});
+
+	mongoose.Promise = global.Promise;
+};
