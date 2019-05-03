b493b94cad671a7cf76d9d0d06efc352072b7161
Muammer Top <muammertop@outlook.com>
2019-05-03 18:40:25 +0300

    Movie validation

--- model/Movie.js
+++ model/Movie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
 	director_id: Schema.Types.ObjectId,
 	title:{
 		type: String,
-		required: true,
+		required: [true, '`{PATH}` alanı zorunludur.'],
+		maxlength: 50,
+		minlength: 1,
 	},
-	category: String,
-	country: String,
-	year: Number,
-	imdb: Number,
+	category: {
+		type: String,
+		maxlength: 30,
+		minlength: 3
+	},
+	country: {
+		type: String,
+		maxlength: 20,
+		minlength: 2,
+	},
+	year: {
+		type: Number,
+		max: 2019,
+		min: 1600,
+	},
+	imdb: {
+		type: Number,
+		max: 10,
+		min: 0
+	},
 	createdAt: {
 		type: Date,
 		default: Date.now
